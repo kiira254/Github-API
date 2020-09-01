e2457cd6c2c8fc27d41d094b1115b5030181e4a2
kiira254 <nkamotho57@gmail.com>
2020-09-01 16:24:27 +0300

    add services

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -8,11 +8,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 export class UserService {
   userName: string;
   repoName: string;
-  clientId: string= 'dc4cbc3dcccd8cfa4ec7';
+  clientId: string= '"MDQ6VXNlcjY3Njg5Nzk0"';
   clientSecret: string = '5dfa88d45fa23b6c7ee535af8844baccfd42e61d';
-  apikey: string = '2acdeec577f9c9bdce8b7dde1cbf27e1b5134f14'
+  apikey: string = 'd096e17415f37a53e8e6e7b11da78963dc315340'
 
   constructor(private http: HttpClient) {
     console.log('service is now ready');
   }
+  getProfileInfo(){
+   return this.http.get("https://api.github.com/users/" + this.userName + "?client_id=" + this.clientId + "&client_secret=" + this.clientSecret);
+ 
+ }
+ getProfileRepos(){
+   return this.http.get("https://api.github.com/users/" + this.userName + "/repos?client_id=" + this.clientId + "&client_secret=" + this.clientSecret);
+ }
+ searchrepos() {
+   return this.http.get('https://api.github.com/search/repositories?q=' + this.repoName, ({
+     headers: new HttpHeaders({Authorization: `token ${this.apikey}`})
+   }))
+ }
+ updateProfile(userName:string){
+   this.userName =userName
+ }
+ UpdateRepo(repoName:string) {
+   this.repoName = repoName;
+ }
+
 }
