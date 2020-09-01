e51d9004bcf5736618e7865a90e0ba8600f78fa4
kiira254 <nkamotho57@gmail.com>
2020-09-01 15:01:03 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RepoComponent } from './repo/repo.component';
+import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RepoComponent,
+    SearchComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
