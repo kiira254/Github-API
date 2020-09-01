d321fbceaed6c8882e3d423f2d7f77ec1ba17963
kiira254 <nkamotho57@gmail.com>
2020-09-01 15:46:33 +0300

    add navbar

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -12,5 +12,7 @@ export class UserService {
   clientSecret: string = '5dfa88d45fa23b6c7ee535af8844baccfd42e61d';
   apikey: string = '2acdeec577f9c9bdce8b7dde1cbf27e1b5134f14'
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log('service is now ready');
+  }
 }
