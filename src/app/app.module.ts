8aeab9fe26141c779d9c8e94ddb93bca00bcd4ec
kiira254 <nkamotho57@gmail.com>
2020-09-01 15:20:00 +0300

    add links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RepoComponent } from './repo/repo.component';
 import { SearchComponent } from './search/search.component';
 import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
+import { HomeComponent } from './home/home.component';
+import { UserService } from './user-service/user.service';
+import { HttpClientModule } from '@angular/common/http';
 import { DateCountPipe } from './date-count.pipe';
-import { HighlightDirective } from './highlight.directive';
+import { HighlightDirective } from './highlight.directive'
 
 @NgModule({
   declarations: [
