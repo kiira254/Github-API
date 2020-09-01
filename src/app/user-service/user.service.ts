828ead060cf672c92bd6fa5a42ae47f87a529745
kiira254 <nkamotho57@gmail.com>
2020-09-01 15:36:09 +0300

    constructor in user service

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -6,6 +6,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 })
 
 export class UserService {
+  userName: string;
+  repoName: string;
+  clientId: string= 'dc4cbc3dcccd8cfa4ec7';
+  clientSecret: string = '5dfa88d45fa23b6c7ee535af8844baccfd42e61d';
+  apikey: string = '2acdeec577f9c9bdce8b7dde1cbf27e1b5134f14'
 
   constructor() { }
 }
